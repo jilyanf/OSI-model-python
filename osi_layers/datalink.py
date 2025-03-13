d56440f62adcf286d97d4fc4f6e8e3d78fa87bbe
Jilliane Ferolino <158802020+jilyanf@users.noreply.github.com>
2025-03-13 18:29:49 +0800

    modified to use dynamic IP and Mac address

--- osi_layers/datalink.py
+++ osi_layers/datalink.py
@@ -1,6 +1,9 @@
+import socket
+import uuid
+
 class DataLinkLayer:
     def __init__(self):
-        self.mac_address = "00:11:22:33:44:55"
+        self.mac_address = self._get_mac_address()
         
     def create_frame(self, data, dest_mac):
         """Create a frame with MAC addressing"""
@@ -15,3 +18,14 @@ class DataLinkLayer:
     def _calculate_checksum(self, data):
         """Calculate simple checksum"""
         return sum(ord(c) for c in str(data)) & 0xFF
+    
+    def _get_mac_address(self):
+        """Get the MAC address of the current machine"""
+        try:
+            # Get the MAC address (hardware address)
+            mac = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) 
+                           for elements in range(0, 48, 8)][::-1])
+            return mac
+        except Exception:
+            # Fallback to default MAC if detection fails
+            return "00:11:22:33:44:55"
--- osi_layers/network.py
+++ osi_layers/network.py
@@ -1,6 +1,8 @@
+import socket
+
 class NetworkLayer:
     def __init__(self):
-        self.ip_address = "192.168.1.1"
+        self.ip_address = self._get_ip_address()
         
     def create_packet(self, frame, dest_ip):
         """Create IP packet"""
@@ -18,3 +20,17 @@ class NetworkLayer:
             packet['ttl'] -= 1
             return packet
         return None
+    
+    def _get_ip_address(self):
+        """Get the IP address of the current machine"""
+        try:
+            # Create a socket connection to get the local IP
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            # Doesn't need to be reachable, just to determine interface IP
+            s.connect(('8.8.8.8', 1))
+            ip = s.getsockname()[0]
+            s.close()
+            return ip
+        except Exception:
+            # Fallback to default IP if detection fails
+            return "192.168.1.1"
